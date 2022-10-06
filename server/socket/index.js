a4fedb2c4cd861dbf5ca0ce1735e691a5d1531c7
Anjal Bam <anjalbam81@gmail.com>
2022-10-06 13:59:08 +0545

    [feat] add always event listener;

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,7 +1,15 @@
+const { authMiddleware } = require("../middlewares/io");
+
 function useSocketIo(io) {
   io.on("connection", (socket) => {
     console.log("Connected ", socket.id);
+
+    socket.onAny((event, ...args) => {
+      console.log(event, args);
+    });
   });
+
+  io.use(authMiddleware);
 }
 
 module.exports = useSocketIo;
