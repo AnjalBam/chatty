4aa0db352c37fb6ec7609b92067ab024b328f0b6
Anjal Bam <anjalbam81@gmail.com>
2022-10-07 18:58:22 +0545

    [feat] fix token malforming issue;

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,19 +1,22 @@
-const {verifyToken} = require('../utils/jwt');
+const { verifyToken } = require("../utils/jwt");
 
 const isAuthenticated = (req, res, next) => {
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).send({ message: "Unauthorized", 'error': 'No token provided' });
-    }
-    try {
-        const decoded = verifyToken(token);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).send({ message: "Unauthorized", error });
-    }
+  const token = req.headers.authorization;
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: "Unauthorized", error: "No token provided" });
+  }
+  try {
+    const tokenPart = token.toString().split(" ")[1]
+    const decoded = verifyToken(tokenPart);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Unauthorized", error });
+  }
 };
 
 module.exports = {
-    isAuthenticated,
+  isAuthenticated,
 };
--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET  = process.env.JWT_SECRET || 'topSecretKey';
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, JWT_SECRET, {expiresIn: '1d'}).toString();
+  return jwt.sign(payload, JWT_SECRET, {expiresIn: '24h'}).toString();
 };
 
 const verifyToken = (token) => {
